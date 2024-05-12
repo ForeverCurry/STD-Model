5387259eb6ff6b81c01ff32002c892b51ec5cf39
SuLY <49095141+ForeverCurry@users.noreply.github.com>
2024-05-12 12:27:31 +0800

    Create wind.py

---
+++ datasets/wind.py
@@ -0,0 +1,25 @@
+"""
+wind Dataset
+"""
+
+import os
+from dataclasses import dataclass
+import pandas as pd
+from common.settings import DATASETS_PATH
+
+DATASET_PATH = os.path.join(DATASETS_PATH, 'wind')
+
+@dataclass()
+class WindDataset:
+    @staticmethod
+    def load():
+        """
+        Load wind dataset.
+        """
+        data = pd.read_csv(os.path.join(DATASET_PATH,'winddata.csv'),
+                          header=0, delimiter=",",index_col=0)
+        return data
+
+
+ 
+
